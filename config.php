162600c9568163b1d6d609a3639ff8076f80da5b
Connor Peoples <angretlam@gmail.com>
2018-06-24 00:04:08 -0500

    Stupid MySQL connetion is "case sensitive". Fixed config file.

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $ROOT_URL = 'https://anothercrmbeta.connorpeoples.com/';
 $DB_SERV = '127.0.0.1';
-$DB_NAME = 'OnDemandJeT';
+$DB_NAME = 'onDemandJet';
 $DB_USER = 'root';
 $DB_PASS = 'P4$$word9522007983';
 
