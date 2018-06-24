03eded9b7fa5a3738be189c8c2cb05824595a068
Connor Peoples <angretlam@gmail.com>
2018-06-24 00:01:08 -0500

    Fixed syntax error in config file. My bad

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $ROOT_URL = 'https://anothercrmbeta.connorpeoples.com/';
-$DB_SERV = '127.0.0.1'
+$DB_SERV = '127.0.0.1';
 $DB_NAME = 'OnDemandJeT';
 $DB_USER = 'root';
 $DB_PASS = 'P4$$word9522007983';
