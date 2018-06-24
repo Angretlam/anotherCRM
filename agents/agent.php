d53d55e69f0f68dcad80ccbf46dc28ac0f05e8c6
Connor Peoples <angretlam@gmail.com>
2018-06-23 21:23:11 -0500

    Update the agent.php file to properly display information.

--- agents/agent.php
+++ agents/agent.php
@@ -74,78 +74,57 @@ while ($stmt->fetch()) {
                   <button type="submit" class="btn btn-info">Update</button>
                 </form>
           </div>' ;
+}
 
-    // Create the agent status form.
-    echo '
-        </form>
-        <div id="addStatus" class="collapse">
-        <form method="POST" action="https://anothercrmbeta.connorpeoples.com/clients/status.php">
-                <input type="hidden" name="ClientID" value="' . $ClientID . '">
-                        <input type="hidden" name="Name" value="' . $Name . '">
-            <div class="form-group">
-                <br />
-                <label>Client Status</label>
-                <select class="custom-select" name="AgentID">
-                <option value="">None</option>';
+// Create the agent status form.
+echo '
+    </form>
+    <div id="addStatus" class="collapse">
+    <form method="POST" action="https://anothercrmbeta.connorpeoples.com/clients/status.php">
+            <input type="hidden" name="ClientID" value="' . $ClientID . '">
+                    <input type="hidden" name="Name" value="' . $Name . '">
+        <div class="form-group">
+            <br />
+            <label>Client Status</label>
+            <select class="custom-select" name="AgentID">
+            <option value="">None</option>';
 
-    // Get the agent status
-    $agent = '';
-    $query = "SELECT (SELECT StatusName from Status where StatusID = ClientStatus.StatusID) AS CStatus FROM ClientStatus WHERE ClientID =" . $ClientID;
-    $stmt  = $link->prepare($query);
-    error_log($ClientID);
-    $stmt->execute();
-    mysqli_stmt_bind_result($stmt, $agentName);
-    while ($stmt->fetch()) {
-        $agent = $agentName;
-    }
+// Get the agent status
+$agent = '';
+$query = "SELECT (SELECT StatusName from Status where StatusID = ClientStatus.StatusID) AS CStatus FROM ClientStatus WHERE ClientID =" . $ClientID;
+$stmt  = $link->prepare($query);
+error_log($ClientID);
+$stmt->execute();
+mysqli_stmt_bind_result($stmt, $agentName);
+while ($stmt->fetch()) {
+    $agent = $agentName;
+}
 
-    // Generate the select options for the agent status
-    $query = "SELECT StatusID, StatusName from Status WHERE StatusID > 0";
-    $stmt  = $link->prepare($query);
-    $stmt->execute();
-    mysqli_stmt_bind_result($stmt, $agentID, $agentName);
+// Generate the select options for the agent status
+$query = "SELECT StatusID, StatusName from Status WHERE StatusID > 0";
+$stmt  = $link->prepare($query);
+$stmt->execute();
+mysqli_stmt_bind_result($stmt, $agentID, $agentName);
 
-    // Iterate through the results. If the results of the select options query
-    // matches the agent status, make that the select value
-    while ($stmt->fetch()) {
-        if ($agentName == $agent) {
-            echo '<option selected value="' . $agentID . '">' . $agentName . '</option>';
-        } else {
-            echo '<option value="' . $agentID . '">' . $agentName . '</option>';
-        }
-    }
-
-    // Close the form
-    echo ' </select><br /><br />
-                        <button type="submit" class="btn btn-info">Update</button>
-        </div>
-        </div>
-        </form>
-          <!-- HEre in end the test code stuff -->
-        </div>
-          </div>
-    ';
-
-
-    $query = "SELECT (SELECT Name FROM Agents WHERE AgentID = Notes.AgentID), Note, Date FROM Notes WHERE ClientID = " . $ClientID . " Order BY Date DESC";
-    $stmt  = $link->prepare($query);
-    $stmt->execute();
-    mysqli_stmt_bind_result($stmt, $AgentID, $Note, $Date);
-
-    while ($stmt->fetch()) {
-
-        echo '
-        <div class="card" style="width: 80%; margin-bottom:10px; margin-left:auto; margin-right:auto;">
-          <div class="card-body">
-            <h5 class="card-title">' . $Date . '</h5>
-            <h6 class="card-title">' . $AgentID . '</h6>
-            <h6>Note</h6>
-            <p>' . $Note . '</p>
-             </div>
-        </div>';
-
+// Iterate through the results. If the results of the select options query
+// matches the agent status, make that the select value
+while ($stmt->fetch()) {
+    if ($agentName == $agent) {
+        echo '<option selected value="' . $agentID . '">' . $agentName . '</option>';
+    } else {
+        echo '<option value="' . $agentID . '">' . $agentName . '</option>';
     }
 }
+
+// Close the form
+echo ' </select><br /><br />
+                    <button type="submit" class="btn btn-info">Update</button>
+    </div>
+    </div>
+    </form>
+      <!-- HEre in end the test code stuff -->
+    </div>
+      </div>';
 
 require('../footer.php');
 ?>
